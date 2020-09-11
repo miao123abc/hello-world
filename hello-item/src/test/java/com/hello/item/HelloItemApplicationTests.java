42e1843f3633d2ad5753cf0d09d38ed553a4ff48
miao <1119706268@qq.com>
2020-09-11 09:41:03 +0800

    rigester complete

---
+++ hello-item/src/test/java/com/hello/item/HelloItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.item;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloItemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- hello-user/src/main/java/com/hello/user/HelloUserApplication.java
+++ hello-user/src/main/java/com/hello/user/HelloUserApplication.java
@@ -3,8 +3,10 @@ package com.hello.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigurationProperties
 public class HelloUserApplication {
 
